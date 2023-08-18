81c3a8fc912dd37ffb0515166a317c60ea114738
Glenn Jacobsen <glenn.jacobsen@ams.no>
2023-08-18 15:52:48 +0200

    Now has a selection for Chatbot mode or Neverending storyteller mode

--- Program.cs
+++ Program.cs
@@ -2,61 +2,132 @@
 using LLama;
 using LLama.Common;
 
-string pathToActionsJson = "Actions.json";
-string pathToCharactersJson = "Characters.json";
-string pathToEmotionsJson = "Emotions.json";
-string pathToGenresJson = "Genres.json";
-string pathToObjectsJson = "Objects.json";
-string pathToPlotTwistsJson = "PlotTwists.json";
-string pathToSettingsJson = "Settings.json";
-string pathToThemesJson = "Themes.json";
-string pathToTimePeriodsJson = "TimePeriods.json";
+string primer = "default";
+Mode mode = Mode.Exit;
 
+int choice = 0;
 
-var actions = Bot_test.DataLoader.Load<Actions>(pathToActionsJson);
-var characters = Bot_test.DataLoader.Load<CharRoot>(pathToCharactersJson);
-var emotions = Bot_test.DataLoader.Load<Emotions>(pathToEmotionsJson);
-var genres = Bot_test.DataLoader.Load<GenereRoot>(pathToGenresJson);
-var objects = Bot_test.DataLoader.Load<Objects>(pathToObjectsJson);
-var plotTwists = Bot_test.DataLoader.Load<PlotTwists>(pathToPlotTwistsJson);
-var settings = Bot_test.DataLoader.Load<SettingsRoot>(pathToSettingsJson);
-var themes = Bot_test.DataLoader.Load<Themesroot>(pathToThemesJson);
-var timePeriods = Bot_test.DataLoader.Load<TimePeriods>(pathToTimePeriodsJson);
+Console.WriteLine("\nPlease select an option:");
+Console.WriteLine("1. ChatBot");
+Console.WriteLine("2. Never-ending Story Teller");
+Console.WriteLine("3. Exit");
 
-var sentenceGenerator = new SentenceGenerator();
+bool result = Int32.TryParse(Console.ReadLine(), out choice);
 
-var generatedText = sentenceGenerator.GenerateSentence(emotions, genres, timePeriods, characters, actions, objects, settings, plotTwists, themes);
+if (!result)
+{
+    Console.WriteLine("Invalid input. Please enter the number corresponding to your choice.");
+}
+else{
+    switch (choice){
+        case 1:
+            Console.WriteLine("You chose ChatBot.");
+            primer = "Transcript of a professional dialogue between Developer1 and Developer2. They are discussing about a project they are both working on.\r\n\r\nDeveloper1: Hello Developer2! How's the project coming along?\r\nDeveloper2: Hi Developer1! Things are progressing well, we're on track. How about your tasks?\r\nDeveloper1: I've been tackling a tricky bug, but making headway. Any updates from client side?\r\nDeveloper2: Yes, they just approved the last set of features we submitted!\r\nDeveloper1:";
+            mode = Mode.ChatBot;
+            break;
+        case 2:
+            Console.WriteLine("You chose Never-ending Story Teller.");
+            
+            while(true)
+            {
+                primer = GetPrimer();
+                Console.WriteLine("Generated Primer: " + primer);
+                Console.WriteLine("Do you want to use this Primer[y/n]:");
+                string response = Console.ReadLine();
+                
+                if(response.Equals("y", StringComparison.OrdinalIgnoreCase))
+                {
+                    mode = Mode.StoryTeller;
+                    break;
+                }
+                else if(!response.Equals("n", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Invalid input. Type 'y' to accept the primer or 'n' to generate a new one");
+                }
+            }
+            break;
+        case 3:
+            Console.WriteLine("Exiting...");
+            mode = Mode.Exit;
+            break;
+        default:
+            Console.WriteLine("Invalid choice. Please select a valid option.");
+            break;
+    }
+}
 
-string modelPath = "C:\\Users\\Glennwiz\\AppData\\Local\\nomic.ai\\GPT4All\\wizardLM-13B-Uncensored.ggmlv3.q4_0.bin";
-var prompt = generatedText;
+var modelPath = "C:\\dev\\LLMs\\llama-2-7b-chat.ggmlv3.q3_K_L.bin";
+var prompt = primer;
 
 // Initialize a chat session //seed 2000, 1.18f
 var ex = new InteractiveExecutor(new LLamaModel(new ModelParams(modelPath, contextSize: 2048, seed: 2001, gpuLayerCount: 5)));
-ChatSession session = new ChatSession(ex);
+var session = new ChatSession(ex);
 
-// show the prompt
 Console.WriteLine();
+//Write the primer
 Console.Write(prompt);
 
-// run the inference in a loop to chat with LLM
-while (prompt != "stop")
-{
-    foreach (var text in session.Chat(prompt, new InferenceParams() { Temperature = 0.6f, AntiPrompts = new List<string> { "\r\n" } }))
+if(mode == Mode.StoryTeller){
+    while (prompt != "stop")
     {
-        Console.Write(text);
-    }
+        foreach (var text in session.Chat(prompt,
+                     new InferenceParams() { Temperature = 0.6f, AntiPrompts = new List<string> { "\r\n" } }))
+        {
+            Console.Write(text);
+        }
 
-    prompt = ".";
-    //var key = Console.ReadKey(intercept: true); // Read the key without displaying it
-    //if (key.KeyChar == '.')
-    //{
-    //    prompt = Console.ReadLine();
-    //}
-    //else if (key.KeyChar != '\r' && key.KeyChar != '\n') // To ensure that only characters other than Enter are processed
-    //{
-    //    prompt += key.KeyChar; // Build the prompt string until 'stop' is recognized
-    //}
+        prompt = ".";
+    }
+}
+else if(mode == Mode.ChatBot){
+    while (true)
+    {
+        foreach (var text in session.Chat(prompt, new InferenceParams() { Temperature = 0.6f, AntiPrompts = new List<string> { "Developer1:" } }))
+        {
+            Console.Write(text);
+        }
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        prompt = Console.ReadLine();
+        Console.ForegroundColor = ConsoleColor.White;
+
+    }
 }
 
 // save the oki 
 session.SaveSession("SavedSessionPath");
+
+static string GetPrimer()
+{
+    var pathToActionsJson = "Actions.json";
+    var pathToCharactersJson = "Characters.json";
+    var pathToEmotionsJson = "Emotions.json";
+    var pathToGenresJson = "Genres.json";
+    var pathToObjectsJson = "Objects.json";
+    var pathToPlotTwistsJson = "PlotTwists.json";
+    var pathToSettingsJson = "Settings.json";
+    var pathToThemesJson = "Themes.json";
+    var pathToTimePeriodsJson = "TimePeriods.json";
+
+    var themes = Bot_test.DataLoader.Load<Themesroot>(pathToThemesJson);
+    var genres = Bot_test.DataLoader.Load<GenereRoot>(pathToGenresJson);
+    var objects = Bot_test.DataLoader.Load<Objects>(pathToObjectsJson);
+    var actions = Bot_test.DataLoader.Load<Actions>(pathToActionsJson);
+    var settings = Bot_test.DataLoader.Load<SettingsRoot>(pathToSettingsJson);
+    var emotions = Bot_test.DataLoader.Load<Emotions>(pathToEmotionsJson);
+    var characters = Bot_test.DataLoader.Load<CharRoot>(pathToCharactersJson);
+    var plotTwists = Bot_test.DataLoader.Load<PlotTwists>(pathToPlotTwistsJson);
+    var timePeriods = Bot_test.DataLoader.Load<TimePeriods>(pathToTimePeriodsJson);
+
+    var sentenceGenerator = new SentenceGenerator();
+
+    var generatedText = sentenceGenerator.GenerateSentence(emotions, genres, timePeriods, characters, actions, objects, settings, plotTwists, themes);
+    return generatedText;
+}
+
+enum Mode
+{
+    ChatBot,
+    StoryTeller,
+    Exit
+}
