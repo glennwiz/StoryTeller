f13616f1e8ed9c9419ce7eb9fa01ee7376a00f1a
Glenn <glennwiz@gmail.com>
2023-09-05 20:26:47 +0200

    Add detailed README and adjust session parameters
    Added a comprehensive README detailing the various fields and parameters used in the `InferenceParams` class for the LLM inference bot alongside an image for clear visual representation.
    
    The parameters for `MaxTokens` and `FrequencyPenalty` in the `Sessions.cs` file have been adjusted for better performance and an explanation for each field has been provided for greater clarification.
    
    The `MaxTokens` parameter was changed to `-1` to allow unrestricted token generation until completion. The `FrequencyPenalty` has been increased to `1.2f` to reduce token repetition. The explanations provided aim to aid developers in understanding the effect of each parameter on the bot's output.

--- Sessions/Sessions.cs
+++ Sessions/Sessions.cs
@@ -46,11 +46,25 @@ public class Sessions
 
         var chat = ChatSession?.Chat(primer, new InferenceParams()
         {
-            MaxTokens = 200, 
+            MaxTokens = -1, 
             PathSession = @"c:\dev\LLMs\",
-            Temperature = 0.9f, 
-            TopK = 100,
-            FrequencyPenalty = 0.9f,
+            Temperature = 0.9f, /*The temperature controls the degree of randomness in token selection. 
+            The temperature is used for sampling during response generation, which occurs when topP and topK are applied. 
+            Lower temperatures are good for prompts that require a more deterministic/less open-ended response, while higher temperatures can lead to more diverse or creative results. 
+            A temperature of 0 is deterministic, meaning that the highest probability response is always selected.*/
+            
+            TopK = 100, /* The topK parameter changes how the model selects tokens for output. 
+            A topK of 1 means the selected token is the most probable among all the tokens in the model’s vocabulary (also called greedy decoding),
+            while a topK of 3 means that the next token is selected from among the 3 most probable using the temperature. 
+            For each token selection step, the topK tokens with the highest probabilities are sampled. 
+            Tokens are then further filtered based on topP with the final token selected using temperature sampling.*/
+            
+            TopP = 1.0f, /* The topP parameter changes how the model selects tokens for output. 
+            Tokens are selected from the most to least probable until the sum of their probabilities equals the topP value. 
+            For example, if tokens A, B, and C have a probability of 0.3, 0.2, and 0.1 and the topP value is 0.5, 
+            then the model will select either A or B as the next token by using the temperature and exclude C as a candidate. 
+            The default topP value is 0.95.*/
+            FrequencyPenalty = 1.2f, /*Negative values can be used to increase the likelihood of repetition.*/
             AntiPrompts = new List<string>
             {
                 discordUsername + ":",
@@ -59,7 +73,7 @@ public class Sessions
                 "\n"+discordUsername+":", 
                 "\n*"+discordUsername, 
                 "\n\r\n\r\n\r",
-                "\u003C/s\u003E\u003C/p\u003E\n\n"
+                "\u003C/s\u003E\u003C/p\u003E\n\n",
             }
         });
         
