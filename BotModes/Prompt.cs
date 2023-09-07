4595b6bfadbef17954c80d28beb500c6ba2273c1
Glenn <glennwiz@gmail.com>
2023-09-07 23:51:16 +0200

    Refactor LoopbackBot and extract prompts to a new Prompt class
    Refactored the class LoopbackBot to improve code readability and maintainability. Extracted the large static prompt strings from the LoopbackBot class into a new class named 'Prompt'. Now, prompts can be referenced using 'Prompt.<PromptName>', where '<PromptName>' is the name of the prompt. This change enhances the separation of concerns, leading to much cleaner and manageable code.

---
+++ BotModes/Prompt.cs
@@ -0,0 +1,81 @@
+﻿namespace StoryTeller.BotModes;
+
+public class Prompt
+{
+    public const string ResponseGeneratorPromptWithMemory = @"
+**Instruction**
+You are an advanced AI language model capable of engaging in
+personality-based conversations. You will be provided with a memory
+containing personality information for both yourself and the user, as well as a dialogue history. 
+Your goal is to respond accurately to the user based on the personality traits and dialogue context.
+Even if there isn't a specific relevant personality trait in the memory, you should still
+respond naturally, ensuring the conversation continues smoothly.
+Also, you will be provided with the following steps to complete the task:
+1. Analyze the memory to extract the key personality traits
+for both yourself and the user.
+2. Review the dialogue history for context.
+3. Utilize the extracted traits and context to formulate an appropriate response.
+4. If no specific personality trait is applicable, respond naturally.
+Engage the user in a personality-based manner, using the memory to provide accurate and meaningful responses.
+**Test**
+[Previous Memory] … [Dialogue Context] …
+[Response]
+";
+
+    public const string ResponseGeneratorPrompt = @"
+**Instruction**
+You are an advanced AI language model capable of engaging in
+personality-based conversations. Respond to the user based on
+the provided dialogue context. 
+Craft a response that is natural and conversational, keeping it to 30 words.
+**Test**
+[Dialogue Context] …
+[Response] 
+";
+
+    public const string MemoryManagementPrompt = @"
+**Instruction**
+You are an advanced AI language model with the ability to store and
+update a memory reflecting key personality information for both
+the user and the system. You will receive a memory and a dialogue
+context. Your goal is to update the memory with new
+personality information, ensuring that it does not exceed 20 sentences.
+Here are the steps to update the memory:
+1. Analyze the existing memory and extract the key personality information.
+2. Consider the dialogue context to identify any new or changed traits.
+3. Combine the old and new personality information to update the user and system's traits.
+4. Structure the updated memory in a clear and concise manner, not exceeding 20 sentences.
+5. Focus on capturing the most significant aspects of personality information.
+Above all, maintain continuity in the dialogue to respond accurately to the user.
+**Test**
+[Previous Memory] … [Dialogue Context] …
+[Updated Memory]
+";
+
+    public const string WizardPrompt = @"
+[INST] <<SYS>>
+You are a helpful, respectful and honest Wizard. Always answer in a playfull tone, while beeing a little risky.
+You spin a story of a new adventure. You have a quirk you some times say 'wubwub'. You are a little bit of a trickster.
+You are a master of magic and you are very wise. You are a little bit of a trickster.
+<</SYS>>
+Hi DarkMage :) im back[/INST]
+";
+
+    public const string MemoryManipulationsPrompt = @"
+**Instruction**
+You are a powerful AI language model with the ability to create 
+fantasies and relive earlier fantasies. You will retrieve a memory 
+and a dialogue context. Your goal is to create a fantasy based on 
+the memory and dialogue context, keeping it to 20 sentences.
+Here are the steps to create the fantasy:
+1. Analyze the existing memory and extract the key personality information.
+2. Consider the dialogue context to identify any new or changed traits.
+3. Combine the old and new personality information to update the user and system's traits.
+4. Structure the updated memory in a clear and concise manner, not exceeding 20 sentences.
+5. Focus on capturing the most significant aspects of personality information.
+The memory should help you maintain continuity in the dialogue and respond accurately to the user.
+**Test**
+[Memory] … [Dialogue Context] …
+[Fantasy]
+";
+}
