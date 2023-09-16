2c481403df2d1e403a04969116f77ee2da9e376e
Glenn Jacobsen <glennwiz@gmail.com>
2023-09-16 15:51:39 +0200

    Added a target template for Falcon40b params

---
+++ Sessions/Tryout/RunMeTestNewLLM.cs
@@ -0,0 +1,83 @@
+target = "Falcon40BInstruct";
+
+prompth1 = """
+
+Prompt:
+USER: Is it safe to drink water from a dehumidifer?
+ASSISTANT:
+Result:
+It is not recommended to drink water from a
+dehumidifier. Dehumidifiers are designed to remove
+moisture from the air and the water that collects in
+the condensate tank may contain contaminants from
+the air. It is best to use filtered water or bottled water
+for drinking purposes.
+
+
+"""
+
+prompt2 = """
+
+Prompt:
+USER: Is it possible to practice law in any of the US
+states w/o having gone to law school?
+ASSISTANT:
+Result:
+Yes, it is possible to practice law in some US states
+without going to law school through a process called
+"reading the law." This usually involves studying with a
+licensed lawyer for a certain period of time and
+passing a bar exam. It is important to note, however,
+that this process varies by state and may not be
+available in all state
+
+""";
+
+promp3 = """
+
+Prompt:
+USER: What is the relative atomic
+mass of Thallium?
+ASSISTANT:
+Result:
+The relative atomic mass of
+Thallium is 204.383
+
+""";
+
+
+prompt4 = """
+
+Prompt:
+USER: What is the capital city of
+Australia?
+ASSISTANT:
+Result:
+The capital city of Australia is
+Canberra.
+
+""";
+
+prompth5 = """
+
+Prompt:
+USER: In what year was the first
+iPhone released?
+ASSISTANT:
+Result:
+The first iPhone was released in
+2007.
+
+""";
+
+prompt6 = """
+
+Prompt:
+USER: What is the world's largest
+ocean?
+ASSISTANT:
+Result:
+The worlds largest ocean is the
+Pacific Ocean.
+
+""";
