1fc1e94b34adefb51ba301392a2ba42f18a72458
Glenn <glennwiz@gmail.com>
2023-09-26 23:18:07 +0200

    Add TestRunner for language model tests
    A new class TestRunner is added to handle language model tests. The class takes in a JSON file named "language_model_tests.json", parses it, and stores the "prompts" in a List. GetCurrentString method is provided to iterate over the prompts. It will aid in streamlined testing of the language model.

---
+++ TestRunner.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace StoryTeller;
+
+public class LanguageModelTests
+{
+    readonly string json = File.ReadAllText("language_model_tests.json");
+    
+    private int index;
+    private readonly List<string> prompts;
+    
+    public LanguageModelTests()
+    {
+        ResetIndex();
+        dynamic data = JsonConvert.DeserializeObject(json) ?? throw new InvalidOperationException();
+        prompts = data["prompts"].ToList();
+    }
+    
+    private void ResetIndex()
+    {
+        index = 0;
+    }
+
+    public string GetNextString()
+    {
+        return prompts[index++];
+    }
+}
